133d9b2e7e90e84d544a06791aa27a1b205ac9d2
rohittiwari833 <rohittiwari7262@gmail.com>
2022-03-16 21:35:17 +0530

    changed input search

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,38 +1,38 @@
-import React from "react";
-import "./Search.css";
+import React from 'react'
+import './Search.css'
 
 const Search = () => {
   return (
-    <div className="bg-image">
-      <div className="search-section">
-        <div className="search-content">
+    <div className='bg-image'>
+      <div className='search-section'>
+        <div className='search-content'>
           <h1>If you are looking for Job we are here to help you.</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
-        <div className="job-search">
-          <div className="job-title-search">
-            <i class="fa fa-search"></i>
+        <div className='job-search'>
+          <div className='job-title-search box'>
+            <i class='fa fa-search'></i>
             <input
-              type="text"
-              alt="Jobtitle"
-              placeholder="jobtitle,Skills,Companies"
-              className="input-search"
+              type='text'
+              alt='Jobtitle'
+              placeholder='jobtitle,Skills,Companies'
+              className='input-search'
             ></input>
           </div>
-          <div className="job-location">
-            <i class="fa fa-map-marker loc-mr"></i>
+          <div className='job-location box'>
+            <i class='fa fa-map-marker loc-mr'></i>
             <input
-              type="text"
-              alt="Jobloaction"
-              placeholder="location..."
-              className="input-search"
+              type='text'
+              alt='Jobloaction'
+              placeholder='location...'
+              className='input-search'
             ></input>
           </div>
-          <button className="btn">Search</button>
+          <button className='btn'>Search</button>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Search;
+export default Search
